88d1c7e8839abe7d22742a01953a44046593e094
shaofeng <shaofeng@weimi.me>
2015-06-10 11:48:19 +0800

    log json

--- junesix-frame/src/main/java/com/junesix/api/filters/RequestLogFilter.java
+++ junesix-frame/src/main/java/com/junesix/api/filters/RequestLogFilter.java
@@ -5,8 +5,11 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayOutputStream;
+import javax.servlet.http.HttpServletResponseWrapper;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.Arrays;
 
 /**
  * @author sofn
@@ -17,23 +20,9 @@ public class RequestLogFilter implements Filter {
     private static final Logger LOGGER = LoggerFactory.getLogger("REQUEST");
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res,
-                         FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        res = new HttpServletResponseWrapperURL((HttpServletResponse) res);
         chain.doFilter(req, res);
-        String mediaType = response.getContentType();
-        /*try {
-            ServletOutputStream sos = response.getOutputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        res.getOutputStream().write(baos.toByteArray());
-        LOGGER.info(baos.toString());
-//        if (MediaType.APPLICATION_JSON.includes(new MediaType(mediaType))) {
-//            JsonpAdapterWriter writer = new JsonpAdapterWriter(req, response.getWriter());
-//            res.writer(writer);
-//        }
     }
 
     @Override
@@ -44,64 +33,37 @@ public class RequestLogFilter implements Filter {
     public void init(FilterConfig arg0) throws ServletException {
     }
 
-    /*private static class JsonpAdapterWriter implements ContainerResponseWriter {
-        private ContainerResponseWriter origWriter;
-        private ByteArrayOutputStream baos;
-        private ContainerResponse response;
-        private long contentLength;
-        private ContainerRequest httpRequest;
+    private class HttpServletResponseWrapperURL extends HttpServletResponseWrapper {
 
-        public JsonpAdapterWriter(ContainerRequest httpRequest,
-                                  ContainerResponseWriter origWriter) {
-            this.origWriter = origWriter;
-            this.httpRequest = httpRequest;
+        private HttpServletResponse servletResponse;
+
+        public HttpServletResponseWrapperURL(HttpServletResponse response) {
+            super(response);
+            this.servletResponse = response;
         }
 
+        //TODO json格式不走这块代码
         @Override
-        public OutputStream writeStatusAndHeaders(long contentLength,
-                                                  ContainerResponse response) throws IOException {
-            this.response = response;
-            this.baos = new ByteArrayOutputStream();
-            this.contentLength = contentLength;
-            return baos;
+        public PrintWriter getWriter() throws IOException {
+            return new PrintWriterURL(servletResponse.getWriter());
         }
-
-        @Override
-        public void finish() throws IOException {
-            byte[] entity = baos.toByteArray();
-            boolean success = this.response.getStatus() == 200;
-
-            List<String> jsonpList = httpRequest.getQueryParameters().get("_callback");
-            String jsonpMethod = jsonpList != null && jsonpList.size() > 0 ? jsonpList.get(0) : null;
-
-            //对外输出的状态码恒为 200。为了解决2g网络下移动网关修改错误页面的问题。
-            this.response.setStatus(200);
-            StringBuilder bufBegin = new StringBuilder();
-            if (jsonpMethod != null) {
-                bufBegin.append(jsonpMethod).append("(");
-            }
-            bufBegin.append("{\"apistatus\":").append(success ? 1 : 0)
-                    .append(",\"result\":");
-            byte[] begin = bufBegin.toString().getBytes("UTF-8");
-            StringBuilder bufEnd = new StringBuilder();
-            bufEnd.append("}");
-            if (jsonpMethod != null) {
-                bufEnd.append(")");
-            }
-            byte[] end = bufEnd.toString().getBytes("UTF-8");
-            // 如果contentLength 小于0，则不会在header里输出 content-length
-            if (contentLength > 0) {
-                contentLength = contentLength + begin.length + end.length;
-            }
-            // Write out the headers and buffered entity
-            OutputStream out = origWriter.writeStatusAndHeaders(contentLength,
-                    response);
-            out.write(begin);
-            out.write(entity);
-            out.write(end);
-            origWriter.finish();
-        }
-
     }
-*/
+
+    private class PrintWriterURL extends PrintWriter {
+
+        public PrintWriterURL(Writer out) {
+            super(out);
+        }
+
+        public void write(char buf[], int off, int len) {
+            char[] newbuf = Arrays.copyOfRange(buf, off, off + len);
+            String str = String.copyValueOf(newbuf);
+
+            StringBuffer sb = new StringBuffer(str);
+            sb.append("");
+            newbuf = sb.toString().toCharArray();
+            LOGGER.info(sb.toString());
+            super.write(newbuf, 0, newbuf.length);
+        }
+    }
 }
