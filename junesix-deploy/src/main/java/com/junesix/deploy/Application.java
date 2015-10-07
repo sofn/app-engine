4679ffb2bb96c56ba46a0c60da12b857be5e3fe2
lesofn <lesofn@gmail.com>
2015-10-07 21:20:15 +0800

    db

--- junesix-deploy/src/main/java/com/junesix/deploy/Application.java
+++ junesix-deploy/src/main/java/com/junesix/deploy/Application.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ImportResource;
  */
 @Configuration
 @EnableAutoConfiguration
-@ImportResource("classpath*:spring/spring-context.xml")
+@ImportResource("classpath:spring-context.xml")
 public class Application {
 
     public static void main(String[] args) {
