d71da15ae652bb96e55d4b0c1880804ca2d54fe4
sofn <shaofeng@weimi.me>
2015-10-18 01:05:03 +0800

    添加sitemesh和页面

---
+++ deploy/src/main/java/com/appengine/deploy/SiteMeshConfig.java
@@ -0,0 +1,27 @@
+package com.appengine.deploy;
+
+import org.sitemesh.config.ConfigurableSiteMeshFilter;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.Filter;
+
+/**
+ * Authors: sofn
+ * Version: 1.0  Created at 15-6-8 23:15.
+ */
+@Configuration
+public class SiteMeshConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public FilterRegistrationBean siteMeshFilterChain() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        Filter headerFilter = new ConfigurableSiteMeshFilter();
+        registration.setFilter(headerFilter);
+        registration.setOrder(1);
+        return registration;
+    }
+
+}
