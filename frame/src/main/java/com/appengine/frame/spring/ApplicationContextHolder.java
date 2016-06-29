7194e6e0652b1bdf6ae9254218ba71a8c6bd7a10
sofn <lishaofeng02@meituan.com>
2016-06-29 13:26:15 +0800

    ApplicationContextHolder禁用lazy

--- frame/src/main/java/com/appengine/frame/spring/ApplicationContextHolder.java
+++ frame/src/main/java/com/appengine/frame/spring/ApplicationContextHolder.java
@@ -4,7 +4,8 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +17,8 @@ import java.util.List;
  *
  * @author jolestar
  */
-@Component
+@Service
+@Lazy(false)
 public class ApplicationContextHolder implements ApplicationContextAware {
 
     private static ApplicationContext _context;
